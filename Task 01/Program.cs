5a6e61f21baceffcabc76ba27c1d3e44a52f8ece
TEESLA <TEESLA@DESKTOP-H7V796S>
2025-09-29 12:33:44 +0300

    Add project files.

---
+++ Task 01/Program.cs
@@ -0,0 +1,26 @@
+﻿System.Console.WriteLine("Welcome to Our Carpet Cleaning Service ");
+System.Console.WriteLine("We provide the best cleaning service with the following prices ");
+System.Console.WriteLine("Small carpets: 30$ each");
+System.Console.WriteLine("Large carpets: 35$ each");
+System.Console.WriteLine("Sales Tax Rate is: 6% , This Estimates are valid for 30 days");
+
+System.Console.WriteLine("--------------------------------------------------------------"); 
+
+System.Console.Write("Please enter the number of small carpets to be cleaned: ");
+decimal small = Convert.ToDecimal(Console.ReadLine());
+
+System.Console.Write("Please enter the number of large carpets to be cleaned: ");
+decimal large = Convert.ToDecimal(Console.ReadLine());
+
+System.Console.WriteLine("--------------------------------------------------------------");
+
+decimal total = (small * 30 ) + ( large * 35 );
+decimal tax = (total * 6) / 100;
+decimal price = total + tax;
+
+System.Console.WriteLine($"Carpets cost:{total:C}");
+System.Console.WriteLine($"Tax: {tax:C}");
+System.Console.WriteLine($"Total Cost:{price:C}");
+
+System.Console.WriteLine("Thank you for using Islam's Carpet Cleaning Service!");
+System.Console.WriteLine ("Your order has been received");
