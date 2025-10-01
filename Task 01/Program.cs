bf428ae2b27599be8ad8b32850dcdb7e60350425
TEESLA <TEESLA@DESKTOP-H7V796S>
2025-10-01 20:43:50 +0300

    third change

--- Task 01/Program.cs
+++ Task 01/Program.cs
@@ -1,32 +1,38 @@
 ﻿// Welcome message and App infonramation:---------------------------------------------------
 System.Console.WriteLine("Welcome to Our Carpet Cleaning Service ");
 System.Console.WriteLine("We provide the best cleaning service with the following prices ");
-System.Console.WriteLine("Small carpets: 30$ each");
-System.Console.WriteLine("Large carpets: 35$ each");
-System.Console.WriteLine("Sales Tax Rate is: 6% , This Estimates are valid for 30 days");
+
+const decimal smallCarpetPrice = 30m;
+System.Console.WriteLine($"Small carpets: {smallCarpetPrice:C} each");
+
+const decimal largeCarpetPrice = 35m;
+System.Console.WriteLine($"Large carpets: {largeCarpetPrice:C} each");
+
+const decimal salesTaxRate = 0.06m;
+System.Console.WriteLine($"Sales Tax Rate is: {salesTaxRate:P} , This Estimates are valid for 30 days");
 
 System.Console.WriteLine("--------------------------------------------------------------");
 
 
 //inputs-----------------------------------------------------------------------------------
 System.Console.Write("Please enter the number of small carpets to be cleaned: ");
-decimal small = Convert.ToDecimal(Console.ReadLine());
+int small = Convert.ToInt32(Console.ReadLine());
 
 System.Console.Write("Please enter the number of large carpets to be cleaned: ");
-decimal large = Convert.ToDecimal(Console.ReadLine());
+int large = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("--------------------------------------------------------------");
 
 
 //calculations-----------------------------------------------------------------------------
-decimal total = (small * 30 ) + ( large * 35 );
-decimal tax = (total * 6) / 100;
+decimal total = (small * smallCarpetPrice ) + ( large * largeCarpetPrice );
+decimal tax =  total * salesTaxRate;
 decimal price = total + tax;
 
 
 //outputs----------------------------------------------------------------------------------
-System.Console.WriteLine($"Carpets cost:{total:C}");
-System.Console.WriteLine($"Tax: {tax:C}");
-System.Console.WriteLine($"Total Cost:{price:C}");
+System.Console.WriteLine($"Carpets cost:{total,10:C}");
+System.Console.WriteLine($"Tax: {tax,10:C}");
+System.Console.WriteLine($"Total Cost:{price,10:C}");
 
 System.Console.WriteLine("Thank you for using Islam's Carpet Cleaning Service!");
 System.Console.WriteLine ("Your order has been received");
